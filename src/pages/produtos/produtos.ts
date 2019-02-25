fb3f8add4900e4e79531a1bf9b5870c56ccd88a7
denilso <denilso.brustelo@gmail.com>
2019-02-25 13:37:28 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -4,12 +4,6 @@ import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 
-/**
- * Generated class for the ProdutosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -18,7 +12,8 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page : number = 0;
 
 
   constructor(
@@ -36,11 +31,17 @@ export class ProdutosPage {
   loadData() {
     let categoria_id = this.navParams.get('categoria_id');
       let loader = this.presentLoading();
-      this.produtoService.findByCategoria(categoria_id)
+      this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe( response => {
-        this.items = response['content'];
+        //this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length - 1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        console.log(this.items);
+        //this.loadImageUrls();
+        this.loadImageUrls(start, end);
       },
       error => {
         loader.dismiss();
@@ -48,8 +49,8 @@ export class ProdutosPage {
   }
 
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i=start; i<=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -73,9 +74,19 @@ export class ProdutosPage {
 
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -16,9 +16,14 @@ export class ProdutoService {
     return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
   }
 
-  findByCategoria(categoria_id : string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
-  }
+  //findByCategoria(categoria_id : string) {
+  //  return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  // }
+
+  //Busca paginada
+  findByCategoria(categoria_id : string, page : number = 0, linesPerPage : number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
+  }  
 
   getSmallImageFromBucket(id : string) : Observable<any> {
     let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
